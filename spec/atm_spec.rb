a91ff473c26c8b91f1e8959815465a91ac8035f3
kalle <kmthorsnes@gmail.com>
2018-02-01 20:17:29 +0100

    Added withdraw method passed test

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,26 +1,23 @@
-# I will add comments in this file just for my own referance
-
-# First user story
-# As a User
-# In order to make an withdrawal
-# The ATM needs to have funds
-
 require './lib/atm.rb'
 
 describe Atm do
+  let(:account) { instance_double('Account') }
+  before do
+    allow(account).to receive(:balance).and_return(100)
+    allow(account).to receive(:balance=)
+  end
 
-  it 'has 1000$ on initialize' do
+  it '1000$ on initialize' do
     expect(subject.funds).to eq 1000
   end
 
-  # Second user story
-  # as a User
-  # In order to get cash, my money should be reduced on withdrawal
-  # Atm needs to reduce funds when i withdrawal
-
-  it 'funds are reduced at withdrawal' do
-    subject.withdraw 50
-    expect(subject.funds).to eq 950
+  it 'Funds are reduced at withdraw' do
+      subject.withdraw(50, account)
+      expect(subject.funds).to eq 950
   end
 
+  it 'Allow withdraw if account has enough balance' do
+  expected_output = { status: true, message: 'success', date: Date.today, amount: 45 }
+  expect(subject.withdraw(45, account)).to eq expected_output
+  end
 end
