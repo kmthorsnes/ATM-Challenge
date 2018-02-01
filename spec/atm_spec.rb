9848e5c5dae4e12b9ef13879343e2a12885301ce
kalle <kmthorsnes@gmail.com>
2018-02-01 18:01:52 +0100

    Passed test - funds are reduced at withdrawal

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -8,7 +8,19 @@
 require './lib/atm.rb'
 
 describe Atm do
+
   it 'has 1000$ on initialize' do
     expect(subject.funds).to eq 1000
   end
+
+  # Second user story
+  # as a User
+  # In order to get cash, my money should be reduced on withdrawal
+  # Atm needs to reduce funds when i withdrawal
+
+  it 'funds are reduced at withdrawal' do
+    subject.withdraw 50
+    expect(subject.funds).to eq 950
+  end
+
 end
