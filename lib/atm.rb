c4487dd27a19242fdcfea25c67dc2cc8fb8a6501
kalle <kmthorsnes@gmail.com>
2018-02-01 20:25:42 +0100

    refactoring

--- lib/atm.rb
+++ lib/atm.rb
@@ -8,13 +8,23 @@ class Atm
   end
 
   def withdraw(amount, account)
-      case
-      when amount > account.balance
-        return
-      else
-        @funds -= amount
-        account.balance = account.balance - amount
-        { status: true, message: 'success', date: Date.today, amount: amount }
-      end
+    case
+    when insuffient_funds_in_account?(amount, account)
+      return
+    else
+      perform_transaction(amount, account)
+    end
+  end
+
+private
+
+  def insuffient_funds_in_account?(amount, account)
+    amount > account.balance
+  end
+
+  def perform_transaction(amount, account)
+    @funds -= amount
+    account.balance = account.balance - amount
+    { status: true, message: 'success', date: Date.today, amount: amount}
   end
 end
