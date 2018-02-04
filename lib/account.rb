37871a0571d44621babbc68a00e47bb78e38543b
kalle <kmthorsnes@gmail.com>
2018-02-04 13:57:25 +0100

    passed account tests: 4-digit pin-code, :active on initialize, expiry date on initialize

--- lib/account.rb
+++ lib/account.rb
@@ -1,11 +1,22 @@
-require 'date'
-
 class Account
-attr_accessor :pin_code
+  require 'date'
+  attr_accessor :pin_code, :balance, :account_status
+
+  STANDARD_VALIDITY_YRS = 5
 
   def initialize
     @pin_code = generate_pin
-  def
+    @balance = 0
+    @account_status = :active
+  end
+
+  def exp_date
+    Date.today.next_year(STANDARD_VALIDITY_YRS).strftime("%m/%y")
+  end
+
+  def deactivate
+    @account_status = :deactivated
+  end
 
   private
 
