8ac2f9fc298d22d33d4a3bcfe14edd28b950f926
kalle <kmthorsnes@gmail.com>
2018-02-04 14:28:33 +0100

    passed 8 tests for account

--- lib/account.rb
+++ lib/account.rb
@@ -1,13 +1,14 @@
 class Account
   require 'date'
-  attr_accessor :pin_code, :balance, :account_status
+  attr_accessor :pin_code, :balance, :account_status, :owner
 
   STANDARD_VALIDITY_YRS = 5
 
-  def initialize
+  def initialize(attrs = {})
     @pin_code = generate_pin
     @balance = 0
     @account_status = :active
+    set_owner(attrs[:owner])
   end
 
   def exp_date
@@ -22,5 +23,13 @@ class Account
 
   def generate_pin
     rand(1000..9999)
+  end
+
+  def set_owner(obj)
+    obj == nil ? missing_owner : @owner = obj
+  end
+
+  def missing_owner
+    raise "An account owner is required"
   end
 end
